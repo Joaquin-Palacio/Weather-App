aff4cc542463eb97502858ea4e9a4be4963141c7
Joaquin <joaquinpalacio2311@gmail.com>
2023-01-24 19:17:08 -0300

    proyecto finalizado (base)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import coldImg from './assets/cold.jpg';
+import hotImg from './assets/hot.jpg';
+import { Descriptions } from './components/Descriptions';
+import { getWeatherData } from './WeatherService';
 
 function App() {
+  const [city, setCity] = useState('Córdoba');
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState('metric');
+  const [bg, setBg] = useState(hotImg);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getWeatherData(city, units);
+      setWeather(data);
+
+      // dinamic background
+      const threshold = units === 'metric' ? 20 : 60;
+      if(data.temp <= threshold) setBg(coldImg)
+      else setBg(hotImg);
+    };
+
+    fetchData();
+  }, [units, city]);
+
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+    const isCelsius = currentUnit === 'C';
+    button.innerText = isCelsius ? '°F' : '°C';
+    setUnits(isCelsius ? 'metric' : 'imperial');
+  };
+
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value)
+      e.currentTarget.blur()
+    }
+  }
+
   return (
-    <div className='app' style={{ backgroundImage: `url(${coldImg})` }}>
-        <div className='overlay'>
+    <div className='app' style={{ backgroundImage: `url(${bg})` }}>
+      <div className='overlay'>
+        {weather && (
           <div className='container'>
             <div className='section section__inputs'>
-              <input type='text' name='city' placeholder='Enter City...' />
-              <button>°C</button>
+              <input onKeyDown={enterKeyPressed} type='text' name='city' placeholder='Enter City...' />
+              <button onClick={(e) => handleUnitsClick(e)}>°C</button>
             </div>
             <div className='section section__temperature'>
               <div className='icon'>
-                <h3>Cordoba, Ar</h3>
-                <img
-                  src='https://openweatherapp.org/img/wn/02d@2x.png'
-                  alt='weatherIcon'
-                />
-                <h3>Cloudy</h3>
+                <h3>{`${weather.name}, ${weather.country}`} </h3>
+                <img src={weather.iconURL} alt='weatherIcon' />
+                <h3>{weather.description}</h3>
               </div>
               <div className='temperature'>
-                <h1>32 °C</h1>
+                <h1>{`${weather.temp.toFixed()} °${
+                  units === 'metric' ? 'C' : 'F'
+                }`}</h1>
               </div>
             </div>
+
             {/* bottom description */}
+
+            <Descriptions weather={weather} units={units} />
           </div>
-        </div>
+        )}
       </div>
+    </div>
   );
 }
 
